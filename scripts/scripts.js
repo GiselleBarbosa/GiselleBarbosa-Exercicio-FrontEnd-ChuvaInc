4d5aef293a29cb94475bf44b91338e5e5359e81d
Giselle Barbosa <93397497+GiselleBarbosa@users.noreply.github.com>
2022-05-12 09:42:45 -0300

    Atualização main-content e painel side-right

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+function LeiaMais() {
+  var pontos = document.getElementById("pontos");
+  var maisTexto = document.getElementById("mais");
+  var VerMais = document.getElementById("btnLeiaMais");
+
+  if (pontos.style.display === "none") {
+    pontos.style.display = "inline";
+    maisTexto.style.display = "none";
+    btnLeiaMais.innerHTML = "Ver Mais";
+  } else {
+    pontos.style.display = "none";
+    maisTexto.style.display = "inline";
+    btnLeiaMais.innerHTML = "Ver Menos";
+  }
+}
